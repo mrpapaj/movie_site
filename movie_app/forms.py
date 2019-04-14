c13298c0714ce28f074d188635e1bdb17e539b59
mrpapaj <kevin.nikolas123@gmail.com>
2019-04-15 00:29:24 +0200

    Remove label from form bar

--- movie_app/forms.py
+++ movie_app/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class FormMovie(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(widget=forms.TextInput(attrs={'class' : 'input', 'placeholder': 'Movie title'}), label='')
