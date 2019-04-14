c38b2589ad0b42dfe876071b47a5b23bd178d057
mrpapaj <kevin.nikolas123@gmail.com>
2019-04-14 14:25:27 +0200

    Search movie using 'omdbapi' and display it on site

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,4 +3,5 @@ from movie_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('movies/', views.movie_details, name='movie_details')
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 def index(request):
     my_dict = {'insert_msg': "This is your home page"}
     return render(request, 'movie_app/index.html', context=my_dict)
+
+def movie_details(request):
+    movie_title = "Dragon"
+    response = requests.get("http://www.omdbapi.com/?t=" + movie_title, params={'apikey': "adedb5e4"})
+    data = response.text
+    movie_info = {'insert_movie': data}
+    return render(request, 'movie_app/search_movies.html', context=movie_info)
