5a1d4c48621c2c8e51842d469a9bd6fa451db34d
mrpapaj <kevin.nikolas123@gmail.com>
2019-04-15 15:32:32 +0200

    Add 'logout' url for base.html

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,7 +3,7 @@ from movie_app import views
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('', LoginView.as_view(template_name='registration/login.html'), name='login_page'),
     path('index/', views.index, name='index'),
     path('movie/', views.movie_details, name='search_movies'),
 ]
--- movie_site/settings.py
+++ movie_site/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join('static'), )
 
 LOGIN_REDIRECT_URL = 'search_movies'
+LOGOUT_REDIRECT_URL = 'login_page'
